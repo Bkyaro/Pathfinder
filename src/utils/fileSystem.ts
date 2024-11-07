ef03c28a141c793620348a39732fb442b5e703c2
Jackal_lzt <bkyarokun@gmail.com>
2024-11-07 18:06:01 +0800

    feat: traverse path

---
+++ src/utils/fileSystem.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+import * as path from "path";
+
+interface FileNode {
+	name: string;
+	path: string;
+	type: "file" | "directory";
+	children?: FileNode[];
+}
+
+export function scanDirectory(dirPath: string): FileNode {
+	const stats = fs.statSync(dirPath);
+	const baseName = path.basename(dirPath);
+
+	if (!stats.isDirectory()) {
+		return {
+			name: baseName,
+			path: dirPath,
+			type: "file",
+		};
+	}
+
+	const children = fs.readdirSync(dirPath).map((file) => {
+		const fullPath = path.join(dirPath, file);
+		return scanDirectory(fullPath);
+	});
+
+	return {
+		name: baseName,
+		path: dirPath,
+		type: "directory",
+		children,
+	};
+}
